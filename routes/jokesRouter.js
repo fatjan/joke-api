8c619a46206dfee0d1097c068a92040ebfbf9e1b
Fatma Janna <fatma@alterra.id>
2020-12-04 14:06:08 +0700

    use axios to get joke data from other api

--- routes/jokesRouter.js
+++ routes/jokesRouter.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const axios = require("axios")
+const axios = require('axios')
 const router = express.Router()
 
 const basicAPI = 'http://api.icndb.com/jokes/random/'
@@ -7,8 +7,13 @@ const basicAPI = 'http://api.icndb.com/jokes/random/'
 // get random jokes from other api
 router.get('/from-api', async (req, res) => {
   try {
-    const jokes = await axios.$get(basicAPI)
-    res.status(200).json({ message: 'SUCCESS', data: jokes })
+    await axios.get(basicAPI)
+      .then((response) => {
+        if (response.data.type === 'success') {
+          const obtainedJoke = response.data.value.joke
+          res.status(200).json({ message: 'SUCCESS', data: obtainedJoke })
+        }
+      })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
