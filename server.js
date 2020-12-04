3b3a45668d6f9ac3481075cecf9cc51881cd7420
Fatma Janna <fatma@alterra.id>
2020-12-04 22:22:04 +0700

    get data with pagination using express-paginate

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
 require('dotenv').config()
 const express = require('express')
+const paginate = require('express-paginate')
 const app = express()
+const Jokes = require('./models/jokesModels')
+
+app.get('/jokes/paginate', async (req, res, next) => {
+  const amount = parseInt(req.query.limit)
+  const page = parseInt(req.query.page)
+  try {
+    const [results, itemCount] = await Promise.all([
+      Jokes.find({}).limit(amount).skip(req.skip).lean().exec(),
+      Jokes.countDocuments({})
+    ])
+
+    const pageCount = Math.ceil(itemCount / amount)
+
+    if (req.accepts('json')) {
+    // inspired by Stripe's API response for list objects
+      res.json({
+        object: 'list',
+        page: page,
+        total_page: pageCount,
+        total_item: itemCount,
+        limit: amount,
+        has_more: paginate.hasNextPages(req)(pageCount),
+        data: results
+      })
+    } else {
+      res.render('jokes', {
+        jokes: results,
+        pageCount,
+        itemCount,
+        pages: paginate.getArrayPages(req)(3, pageCount, page)
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+})
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
